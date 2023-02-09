e715d6e8b41c7cf6a1c5b240a20f93d064e30e71
Treilind <thorsten.reisinger@gmx.net>
2023-02-09 21:03:36 +0100

    Add first testcases

---
+++ src/test/java/org/campus02/oop/CountryAnalyzerTest.java
@@ -0,0 +1,12 @@
+package org.campus02.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountryAnalyzerTest {
+
+    @Test
+    void analyze() {
+    }
+}
---
+++ src/test/java/org/campus02/oop/MaxLogEntryAnalyzerTest.java
@@ -0,0 +1,23 @@
+package org.campus02.oop;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxLogEntryAnalyzerTest {
+
+    private ArrayList<LogEntry> logEntries;
+
+    @BeforeEach
+    void setUp() {
+
+
+    }
+
+    @Test
+    void analyze() {
+    }
+}
